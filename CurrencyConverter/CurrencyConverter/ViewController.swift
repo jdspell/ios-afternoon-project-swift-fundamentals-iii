ba5f23f543e056ca3ba3d0ea91b3eb66f33214b6
jdspell <jordanspell@Jordans-iMac.attlocal.net>
2019-06-03 22:08:28 -0400

    created app with ability to convert between pesos/canadian dollars to us dollars

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,13 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrencyType {
+    case cad
+    case peso
+}
 
+class ViewController: UIViewController {
+    
+    
+    let cadExchangeRate = 3.0
+    let pesoExchangeRate = 2.0
+    
+    var currencyType: CurrencyType = .cad
+    
+    @IBOutlet weak var fromCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyTextField: UITextField!
+    
+    @IBOutlet weak var toCurrencyLabel: UILabel!
+    
+    @IBOutlet weak var cadButton: UIButton!
+    @IBOutlet weak var pesoButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //needed to add so that cad was selected on load
+        //should be a way to have button already toggled without this line
+        cadButton.isSelected.toggle()
     }
-
-
+    
+    func convert(dollars: Double, to unit: CurrencyType) -> Double {
+        if currencyType == .cad {
+            return dollars * cadExchangeRate
+        } else {
+            return dollars * pesoExchangeRate
+        }
+    }
+    
+    
+    @IBAction func convertButtonPressed(_ sender: Any) {
+        guard let userInput = fromCurrencyTextField.text, let dollars = Double(userInput) else { return }
+        
+        if cadButton.isSelected {
+            toCurrencyTextField.text = String(convert(dollars: dollars, to: currencyType))
+        } else {
+            toCurrencyTextField.text = String(convert(dollars: dollars, to: currencyType))
+        }
+    }
+    
+    @IBAction func cadButtonPressed(_ sender: Any) {
+        toggleCurrency()
+    }
+    
+    @IBAction func pesoButtonPressed(_ sender: Any) {
+        toggleCurrency()
+    }
+    
+    func toggleCurrency() {
+        cadButton.isSelected.toggle()
+        pesoButton.isSelected.toggle()
+        
+        if cadButton.isSelected {
+            currencyType = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        } else {
+            currencyType = .peso
+            toCurrencyLabel.text = "Currency (Peso)"
+        }
+    }
+    
 }
 
